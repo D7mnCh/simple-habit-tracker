750d7f056e458ab4548653f82617d392e702a20e
D7mnCh <da7manch0@gmail.com>
2026-07-29 20:59:42 +0100

    ceate custum checkbox, instead of current one
    - i forget why i made a new one, i'll guess with the custom one i can
      just change the viuals only on theme rather on the whole ui

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,41 @@
 /*
 - i'll try to finish this project in a week as max
+- i think it's time to introduce some hints from gbt
 */
 
 use eframe::{
-    egui::{self, vec2, Checkbox, Color32, Label, RichText, Ui, Vec2},
+    egui::{self, vec2, Color32, Rect, RichText, Sense, Ui, Vec2},
     Result,
 };
 
 // window parameters
-const WIDTH: f32 = 900.;
-const HEIGHT: f32 = 400.;
-const INNER_SIZE: Vec2 = vec2(WIDTH, HEIGHT);
+const WINDOW_SIZE: Vec2 = vec2(850., 400.);
 
 // left panel parameters
-const LEFT_PANEL_SIZE: f32 = 150.;
-const LEFT_PENEL_HEADER_TEXT_SIZE: f32 = 30.;
+const LEFT_PANEL_SIZE: f32 = 100.;
+const LEFT_PENEL_HEADER_TEXT_SIZE: f32 = 25.;
 const LEFT_PANEL_SPACE_BETWEEN_HEADER_LABELS: f32 = 5.;
 const LEFT_PANEL_HABIT_TEXT_SIZE: f32 = 20.;
 
 // centeral panel parameters
-const SPACE_BETWEEN_CHECKBOXES_HORIZONTAL: f32 = 1.;
-const SPACE_BETWEEN_CHECKBOXES_VERTICAL: f32 = 1.;
-const SPACE_BETWEEN_CHECKBOXES: Vec2 = vec2(
-    SPACE_BETWEEN_CHECKBOXES_HORIZONTAL,
-    SPACE_BETWEEN_CHECKBOXES_VERTICAL,
-);
-const YEAR_DAYS: u16 = 365;
-const DAYS_ROW: u16 = 7;
-const MOUNTHS_COLLUMN: u16 = 12;
+const YEAR_DAYS: u16 = 364; // TODO modify it later
+const WEEK_DAYS_ROW: u16 = 7;
+const YEAER_WEEKS_COLLUMN: u16 = 52;
+// NOTE this is gonna make space for all the widgets (that's bad...)
+const SPACE_BETWEEN_CELLS: Vec2 = vec2(2., -4.);
+const CELL_SIZE: Vec2 = vec2(12., 12.);
 
 fn main() -> Result {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_title("simple habit tracker")
             .with_resizable(false)
-            .with_inner_size(INNER_SIZE),
+            .with_inner_size(WINDOW_SIZE),
         ..Default::default()
     };
 
     eframe::run_native(
-        "My egui App",
+        "simple habit tracker",
         native_options,
         Box::new(|cc| Ok(Box::new(HabitTracker::new(cc)))),
     )?;
@@ -47,18 +43,41 @@ fn main() -> Result {
 }
 
 struct HabitTracker {
+    //habits: Vec<Habit>,
+    // NOTE maybe, but 2 source of info? (try)
+    //selected_habit: Habit,
     habit: Habit,
 }
 
 #[derive(PartialEq)]
 enum Habit {
+    //Read(Vec<Cell>),
+    //Write(Vec<Cell>),
+    //Sport(Vec<Cell>),
     Read,
     Write,
     Sport,
 }
 
+struct Cell {
+    rect: Rect,
+    // if marked == gray ,else green
+    color: Color32,
+}
+
 impl HabitTracker {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        let mut cells: Vec<Cell> = Vec::new();
+        for day in 0..YEAR_DAYS {
+            let cell = Cell {
+                rect: Rect::ZERO,
+                color: Color32::from_gray(40),
+            };
+            cells.push(cell);
+        }
+
+        let habits = vec![Habit::Read, Habit::Write, Habit::Sport];
+
         Self { habit: Habit::Read }
     }
 }
@@ -80,22 +99,44 @@ impl HabitTracker {
 
     fn change_ui_style(ui: &mut Ui) {
         // reduce checkboxes spaces between each other
-        ui.spacing_mut().item_spacing = SPACE_BETWEEN_CHECKBOXES;
+        ui.spacing_mut().item_spacing = SPACE_BETWEEN_CELLS;
     }
 
     // left panel
     fn display_left_panel_header(ui: &mut Ui) {
         let text = "Habits";
-        let label = RichText::new(text).size(LEFT_PENEL_HEADER_TEXT_SIZE);
+        let label = RichText::new(text)
+            .size(LEFT_PENEL_HEADER_TEXT_SIZE)
+            .strong();
 
         ui.heading(label);
     }
 
     fn dispaly_left_panel_widgets(&mut self, ui: &mut Ui) {
-        let read_msg = RichText::new("reading").size(LEFT_PANEL_HABIT_TEXT_SIZE);
-        let write_msg = RichText::new("workout").size(LEFT_PANEL_HABIT_TEXT_SIZE);
-        let sport_msg = RichText::new("writing").size(LEFT_PANEL_HABIT_TEXT_SIZE);
+        // NOTE all habits gonna be blued, cuz habit == Habit::...(if equel then blue)
+        //for habit in self.habits.iter_mut() {
+        //    match habit {
+        //        Habit::Write => {
+        //            let write_msg =
+        //                RichText::new("writing").size(LEFT_PANEL_HABIT_TEXT_SIZE);
+        //            ui.selectable_value(habit, Habit::Write, write_msg);
+        //        }
+        //        Habit::Read => {
+        //            let read_msg =
+        //                RichText::new("reading").size(LEFT_PANEL_HABIT_TEXT_SIZE);
+        //            ui.selectable_value(habit, Habit::Read, read_msg);
+        //        }
+        //        Habit::Sport => {
+        //            let sport_msg =
+        //                RichText::new("workout").size(LEFT_PANEL_HABIT_TEXT_SIZE);
+        //            ui.selectable_value(habit, Habit::Sport, sport_msg);
+        //        }
+        //    };
+        //}
 
+        let write_msg = RichText::new("writing").size(LEFT_PANEL_HABIT_TEXT_SIZE);
+        let read_msg = RichText::new("reading").size(LEFT_PANEL_HABIT_TEXT_SIZE);
+        let sport_msg = RichText::new("workout").size(LEFT_PANEL_HABIT_TEXT_SIZE);
         let _response = ui.selectable_value(&mut self.habit, Habit::Read, read_msg);
         let _response =
             ui.selectable_value(&mut self.habit, Habit::Write, write_msg);
@@ -104,12 +145,17 @@ impl HabitTracker {
     }
 
     // central panel
-    fn dispaly_central_panel_cells(&self, ui: &mut Ui) {
-        for _ in 0..MOUNTHS_COLLUMN {
-            let mut checked = false;
-            // NOTE checked is based on a database for that habit
-            let cell = Checkbox::without_text(&mut checked);
-            let _response = ui.add(cell);
+    // TODO Need to store data somewhere
+    fn dispaly_central_panel_cell(&self, ui: &mut Ui) {
+        // NOTE i can create a rect and then use allocate_rect for sense parameter,
+        // when creating rect i don't need to use ui
+        let (rect, response) = ui.allocate_exact_size(CELL_SIZE, Sense::click());
+        // TODO use self.color
+        ui.painter().rect_filled(rect, 4., Color32::from_gray(40));
+
+        if response.clicked() {
+            ui.painter()
+                .rect_filled(rect, 4., Color32::from_rgb(38, 166, 65));
         }
     }
 }
@@ -139,25 +185,13 @@ impl eframe::App for HabitTracker {
         egui::CentralPanel::default().show(ui, |ui| {
             ui.heading("TODO");
 
-            for day in 0..DAYS_ROW {
+            // NOTE maybe i'll change the logic here to get 365
+            for day in 0..WEEK_DAYS_ROW {
                 ui.vertical(|ui| {
                     ui.horizontal(|ui| {
-                        match day {
-                            1 => {
-                                let response = ui.add(Label::new("Sunday  "));
-                                dbg!(&response.intrinsic_size());
-                            }
-                            3 => {
-                                let response = ui.add(Label::new("Tuesday "));
-                                dbg!(&response.intrinsic_size());
-                            }
-                            5 => {
-                                let response = ui.add(Label::new("Thursday"));
-                                dbg!(&response.intrinsic_size());
-                            }
-                            _ => ui.add_space(52.7),
+                        for week in 0..YEAER_WEEKS_COLLUMN {
+                            self.dispaly_central_panel_cell(ui);
                         }
-                        self.dispaly_central_panel_cells(ui);
                     });
                 });
             }
