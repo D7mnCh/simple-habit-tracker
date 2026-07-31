a98c1f0097e6dabe11c1b2e05c29c9d9cb817607
D7mnCh <da7manch0@gmail.com>
2026-07-31 11:09:53 +0100

    Refactor
    - convert habit from enum to struct to reduce repition

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 */
 
 use eframe::{
-    egui::{self, vec2, Color32, Rect, RichText, Sense, Ui, Vec2},
+    egui::{self, vec2, Color32, RichText, Sense, Ui, Vec2},
     Result,
 };
 
@@ -22,9 +22,35 @@ const YEAR_DAYS: u16 = 365;
 // NOTE this is gonna make space for all the widgets (that's bad...)
 const SPACE_BETWEEN_CELLS: Vec2 = vec2(2., -4.);
 const CELL_SIZE: Vec2 = vec2(12., 12.);
-// NOTE should i make an enum for cell color ?
 const UNMARKED_CELL_COLOR: Color32 = Color32::from_gray(40);
 const MARKED_CELL_COLOR: Color32 = Color32::from_rgb(38, 166, 65);
+
+#[derive(Debug, Clone)]
+
+// TODO
+struct Habit {
+    name: &'static str,
+    cells: Vec<Cell>,
+}
+
+//enum Habit {
+//    Read(Vec<Cell>),
+//    Write(Vec<Cell>),
+//    Sport(Vec<Cell>),
+//}
+
+struct HabitTracker {
+    habits: Vec<Habit>,
+    // neeced for building habit selecter widget
+    selected_habit: &'static str,
+}
+
+// NOTE thinking of not letting user manipulate cells to mark them only with a config file
+#[derive(Clone, PartialEq, Debug)]
+struct Cell {
+    // if marked/clicked then green, else gray
+    color: Color32,
+}
 
 fn main() -> Result {
     let native_options = eframe::NativeOptions {
@@ -43,55 +69,32 @@ fn main() -> Result {
     Ok(())
 }
 
-struct HabitTracker {
-    habits: Vec<Habit>,
-    // neeced for building habit selecter widget
-    selected_habit: Habit,
-}
-
-#[derive(Debug, Clone)]
-enum Habit {
-    Read(Vec<Cell>),
-    Write(Vec<Cell>),
-    Sport(Vec<Cell>),
-}
-
 // NOTE didn't understand the recurion thing here
 impl PartialEq for Habit {
     fn eq(&self, other: &Self) -> bool {
         matches!(
-            (self.which(), other.clone()),
-            (Self::Read(_), Self::Read(_))
-                | (Self::Write(_), Self::Write(_))
-                | (Self::Sport(_), Self::Sport(_))
+            (self.name, other.name),
+            ("sport", "sport") | ("write", "write") | ("read", "read")
         )
     }
 }
 
 impl Habit {
-    fn get_cells_mut(&mut self) -> &mut Vec<Cell> {
-        match self {
-            Self::Read(cells) => cells,
-            Self::Write(cells) => cells,
-            Self::Sport(cells) => cells,
-        }
-    }
-
-    fn which(&self) -> Habit {
-        match self {
-            Self::Read(_) => Self::Read(Vec::new()),
-            Self::Write(_) => Self::Write(Vec::new()),
-            Self::Sport(_) => Self::Sport(Vec::new()),
-        }
-    }
-}
-
-// NOTE thinking of not letting user manipulate cells to mark them only with a config file
-#[derive(Clone, PartialEq, Debug)]
-struct Cell {
-    rect: Rect,
-    // if marked/clicked then green, else gray
-    color: Color32,
+    //fn get_cells_mut(&mut self) -> &mut Vec<Cell> {
+    //    match self {
+    //        Self::Read(cells) => cells,
+    //        Self::Write(cells) => cells,
+    //        Self::Sport(cells) => cells,
+    //    }
+    //}
+    //
+    //fn which(&self) -> Habit {
+    //    match self {
+    //        Self::Read(_) => Self::Read(Vec::new()),
+    //        Self::Write(_) => Self::Write(Vec::new()),
+    //        Self::Sport(_) => Self::Sport(Vec::new()),
+    //    }
+    //}
 }
 
 impl HabitTracker {
@@ -99,26 +102,29 @@ impl HabitTracker {
         let mut cells: Vec<Cell> = Vec::new();
         for _day in 0..YEAR_DAYS {
             let cell = Cell {
-                rect: Rect::ZERO,
+                //rect: Rect::ZERO,
                 color: UNMARKED_CELL_COLOR,
             };
             cells.push(cell);
         }
 
-        //let habits = vec![Habit::Read, Habit::Write, Habit::Sport];
-        let habits = vec![
-            Habit::Read(cells.clone()),
-            Habit::Write(cells.clone()),
-            Habit::Sport(cells.clone()),
-        ];
-
-        // Vec::new() for selected_habit cuz we are only using it for habit selection,
-        //we don't need actually the data
-        let cells: Vec<Cell> = Vec::new();
+        let habit_1 = Habit {
+            name: "reading",
+            cells: cells.clone(),
+        };
+        let habit_2 = Habit {
+            name: "sport",
+            cells: cells.clone(),
+        };
+        let habit_3 = Habit {
+            name: "writing",
+            cells: cells.clone(),
+        };
+        let habits = vec![habit_1.clone(), habit_2, habit_3];
 
         Self {
             habits,
-            selected_habit: Habit::Read(cells),
+            selected_habit: habit_1.name,
         }
     }
 }
@@ -151,112 +157,37 @@ impl HabitTracker {
 
     fn dispaly_left_panel_widgets(&mut self, ui: &mut Ui) {
         for habit in self.habits.iter() {
-            match habit {
-                Habit::Write(_) => {
-                    let write_msg =
-                        RichText::new("writing").size(LEFT_PANEL_HABIT_TEXT_SIZE);
-                    let _response = ui.selectable_value(
-                        &mut self.selected_habit,
-                        Habit::Write(Vec::new()),
-                        write_msg,
-                    );
-                }
-                Habit::Read(_) => {
-                    let read_msg =
-                        RichText::new("reading").size(LEFT_PANEL_HABIT_TEXT_SIZE);
-                    let _response = ui.selectable_value(
-                        &mut self.selected_habit,
-                        Habit::Read(Vec::new()),
-                        read_msg,
-                    );
-                }
-                Habit::Sport(_) => {
-                    let sport_msg =
-                        RichText::new("sport").size(LEFT_PANEL_HABIT_TEXT_SIZE);
-                    let _response = ui.selectable_value(
-                        &mut self.selected_habit,
-                        Habit::Sport(Vec::new()),
-                        sport_msg,
-                    );
-                }
-            };
+            let habit_label =
+                RichText::new(habit.name).size(LEFT_PANEL_HABIT_TEXT_SIZE);
+            let _response = ui.selectable_value(
+                &mut self.selected_habit,
+                habit.name,
+                habit_label,
+            );
         }
     }
 
     // central panel
-    // NOTE display from name should not take &mut self?, maybe this is allowed cuz
-    //i am using an immediate mode gui library
     fn dispaly_central_panel_cells(&mut self, ui: &mut Ui) {
-        match &self.selected_habit {
-            Habit::Write(_) => {
-                for habit in self.habits.iter_mut() {
-                    if *habit == self.selected_habit {
-                        let cells = habit.get_cells_mut();
-                        for cell in cells {
-                            let (rect, response) =
-                                ui.allocate_exact_size(CELL_SIZE, Sense::click());
-                            cell.rect = rect;
-                            ui.painter().rect_filled(rect, 4., cell.color);
+        let habit_cells = self
+            .habits
+            .iter_mut()
+            .find(|habit| habit.name == self.selected_habit);
 
-                            if response.clicked() {
-                                if cell.color == UNMARKED_CELL_COLOR {
-                                    cell.color = MARKED_CELL_COLOR;
-                                    ui.painter().rect_filled(rect, 4., cell.color);
-                                } else {
-                                    cell.color = UNMARKED_CELL_COLOR;
-                                    ui.painter().rect_filled(rect, 4., cell.color);
-                                }
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
-            Habit::Sport(_) => {
-                for habit in self.habits.iter_mut() {
-                    if *habit == self.selected_habit {
-                        let cells = habit.get_cells_mut();
-                        for cell in cells {
-                            let (rect, response) =
-                                ui.allocate_exact_size(CELL_SIZE, Sense::click());
-                            cell.rect = rect;
-                            ui.painter().rect_filled(rect, 4., cell.color);
+        if let Some(habit) = habit_cells {
+            let cells = &mut habit.cells;
+            for cell in cells {
+                let (rect, response) =
+                    ui.allocate_exact_size(CELL_SIZE, Sense::click());
+                ui.painter().rect_filled(rect, 4., cell.color);
 
-                            if response.clicked() {
-                                if cell.color == UNMARKED_CELL_COLOR {
-                                    cell.color = MARKED_CELL_COLOR;
-                                    ui.painter().rect_filled(rect, 4., cell.color);
-                                } else {
-                                    cell.color = UNMARKED_CELL_COLOR;
-                                    ui.painter().rect_filled(rect, 4., cell.color);
-                                }
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
-            Habit::Read(_) => {
-                for habit in self.habits.iter_mut() {
-                    if *habit == self.selected_habit {
-                        let cells = habit.get_cells_mut();
-                        for cell in cells {
-                            let (rect, response) =
-                                ui.allocate_exact_size(CELL_SIZE, Sense::click());
-                            cell.rect = rect;
-                            ui.painter().rect_filled(rect, 4., cell.color);
-
-                            if response.clicked() {
-                                if cell.color == UNMARKED_CELL_COLOR {
-                                    cell.color = MARKED_CELL_COLOR;
-                                    ui.painter().rect_filled(rect, 4., cell.color);
-                                } else {
-                                    cell.color = UNMARKED_CELL_COLOR;
-                                    ui.painter().rect_filled(rect, 4., cell.color);
-                                }
-                            }
-                        }
-                        break;
+                if response.clicked() {
+                    if cell.color == UNMARKED_CELL_COLOR {
+                        cell.color = MARKED_CELL_COLOR;
+                        ui.painter().rect_filled(rect, 4., cell.color);
+                    } else {
+                        cell.color = UNMARKED_CELL_COLOR;
+                        ui.painter().rect_filled(rect, 4., cell.color);
                     }
                 }
             }
